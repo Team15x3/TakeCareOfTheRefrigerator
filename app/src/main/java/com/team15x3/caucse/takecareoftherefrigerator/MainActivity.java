9b094b6bc4f85cf39c01d8d4b7da17d9f20a30e2
rina2 <32057202+rina2@users.noreply.github.com>
2018-09-18 16:53:58 +0900

    Get Barcode number

--- app/src/main/java/com/team15x3/caucse/takecareoftherefrigerator/MainActivity.java
+++ app/src/main/java/com/team15x3/caucse/takecareoftherefrigerator/MainActivity.java
@@ -2,16 +2,26 @@ package com.team15x3.caucse.takecareoftherefrigerator;
 
 import android.app.TabActivity;
 import android.content.Intent;
+import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 
 import com.google.zxing.integration.android.IntentIntegrator;
+import com.google.zxing.integration.android.IntentResult;
 
 public class MainActivity extends AppCompatActivity {
 
     Button btnOpenBarcode;
+    String myBarcode ="";
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        IntentResult result = IntentIntegrator.parseActivityResult(requestCode, resultCode,data);
+        myBarcode = result.getContents();
+    }
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -23,6 +33,7 @@ public class MainActivity extends AppCompatActivity {
             public void onClick(View view) {
                 new IntentIntegrator(MainActivity.this).initiateScan();
             }
+
 
         });
 
