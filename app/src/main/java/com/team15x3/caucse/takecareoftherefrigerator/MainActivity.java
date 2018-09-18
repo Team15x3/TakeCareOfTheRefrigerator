2f412e4e49c2ae0ce653238da6f45acec911b133
rina2 <32057202+rina2@users.noreply.github.com>
2018-09-18 16:33:30 +0900

    read Barcode

--- app/src/main/java/com/team15x3/caucse/takecareoftherefrigerator/MainActivity.java
+++ app/src/main/java/com/team15x3/caucse/takecareoftherefrigerator/MainActivity.java
@@ -23,7 +23,7 @@ public class MainActivity extends AppCompatActivity {
             public void onClick(View view) {
                 new IntentIntegrator(MainActivity.this).initiateScan();
             }
-            
+
         });
 
 
