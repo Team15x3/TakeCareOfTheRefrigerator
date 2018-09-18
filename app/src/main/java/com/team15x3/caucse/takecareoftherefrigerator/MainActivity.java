b5e7ccf9a2b817e7ffbb186f88f4bd488e453d61
rina2 <32057202+rina2@users.noreply.github.com>
2018-09-18 16:27:38 +0900

    read Barcode

--- app/src/main/java/com/team15x3/caucse/takecareoftherefrigerator/MainActivity.java
+++ app/src/main/java/com/team15x3/caucse/takecareoftherefrigerator/MainActivity.java
@@ -1,13 +1,32 @@
 package com.team15x3.caucse.takecareoftherefrigerator;
 
+import android.app.TabActivity;
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import com.google.zxing.integration.android.IntentIntegrator;
 
 public class MainActivity extends AppCompatActivity {
 
+    Button btnOpenBarcode;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        btnOpenBarcode = (Button)findViewById(R.id.btnOpenBarcode);
+        btnOpenBarcode.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                new IntentIntegrator(MainActivity.this).initiateScan();
+            }
+            
+        });
+
+
     }
+
 }
