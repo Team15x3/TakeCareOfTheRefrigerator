f3a4947e668bd81dbb9fb2be0400bd045d2099dd
rina2 <32057202+rina2@users.noreply.github.com>
2018-09-18 17:16:24 +0900

    make InfoGetter class, that get information using API

---
+++ app/src/main/java/com/team15x3/caucse/takecareoftherefrigerator/InfoGetter.java
@@ -0,0 +1,17 @@
+package com.team15x3.caucse.takecareoftherefrigerator;
+
+import android.os.AsyncTask;
+
+//The class that get the information of ingredient, with barcode number
+public class InfoGetter extends AsyncTask<String,Void,String> {
+
+    String clientKye = "4ac36909-4c11-4cd6-ae3f-c0e2260c830b";
+    private String str, receivMsg;
+
+    @Override
+    protected String doInBackground(String... strings) {
+        return null;
+    }
+    //private final String ID  = ""
+
+}
