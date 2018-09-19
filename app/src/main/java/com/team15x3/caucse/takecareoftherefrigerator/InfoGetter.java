87483dd138d15bc8515032aa376b015226fbf412
rina2 <32057202+rina2@users.noreply.github.com>
2018-09-19 22:40:57 +0900

    fix error

--- app/src/main/java/com/team15x3/caucse/takecareoftherefrigerator/InfoGetter.java
+++ app/src/main/java/com/team15x3/caucse/takecareoftherefrigerator/InfoGetter.java
@@ -1,17 +1,51 @@
 package com.team15x3.caucse.takecareoftherefrigerator;
 
 import android.os.AsyncTask;
+import android.util.Log;
+import android.widget.Toast;
+
+import java.io.IOException;
+import java.io.OutputStream;
+import java.net.MalformedURLException;
+import java.net.URL;
+
+import javax.net.ssl.HttpsURLConnection;
 
 //The class that get the information of ingredient, with barcode number
 public class InfoGetter extends AsyncTask<String,Void,String> {
 
+    private String barcode = "";
     String clientKye = "4ac36909-4c11-4cd6-ae3f-c0e2260c830b";
     private String str, receivMsg;
 
     @Override
     protected String doInBackground(String... strings) {
+        URL eatSight = null;
+        try{
+            eatSight  =new URL("https://apis.eatsight.com/foodinfo/1.0/foods");
+            HttpsURLConnection myConnection = (HttpsURLConnection)eatSight.openConnection();
+            myConnection.setRequestProperty("User-Agent","test");
+            myConnection.setRequestProperty("Content-Type","application/json");
+            myConnection.setDoOutput(true);
+            myConnection.setDoInput(true);
+
+            OutputStream os = myConnection.getOutputStream();
+            int responseCode = myConnection.getResponseCode();
+            if(responseCode == 200){
+                Log.i("check","success");
+            }else{
+                Log.i("check","fail");
+            }
+        }catch(MalformedURLException e){
+            e.printStackTrace();
+        }catch(IOException e){
+            e.printStackTrace();
+        }
         return null;
     }
     //private final String ID  = ""
 
+    public void setBarcodeNumber(String barcode){
+        this.barcode = barcode;
+    }
 }
--- app/src/main/java/com/team15x3/caucse/takecareoftherefrigerator/MainActivity.java
+++ app/src/main/java/com/team15x3/caucse/takecareoftherefrigerator/MainActivity.java
@@ -2,30 +2,51 @@ package com.team15x3.caucse.takecareoftherefrigerator;
 
 import android.app.TabActivity;
 import android.content.Intent;
+import android.icu.util.Output;
+import android.os.AsyncTask;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.Toast;
 
 import com.google.zxing.integration.android.IntentIntegrator;
 import com.google.zxing.integration.android.IntentResult;
 
-public class MainActivity extends AppCompatActivity {
+import java.io.BufferedInputStream;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.net.URLConnection;
+import java.util.concurrent.ExecutionException;
 
+import javax.net.ssl.HttpsURLConnection;
+
+import static android.util.Log.e;
+
+public class MainActivity extends AppCompatActivity {
+    String myBarcode;
     Button btnOpenBarcode;
-    String myBarcode ="";
+
     @Override
     protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
         super.onActivityResult(requestCode, resultCode, data);
         IntentResult result = IntentIntegrator.parseActivityResult(requestCode, resultCode,data);
-        myBarcode = result.getContents();
+        myBarcode = result.getContents(); //get barcode number
+        Toast.makeText(this,""+myBarcode,Toast.LENGTH_SHORT).show();
     }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
 
         btnOpenBarcode = (Button)findViewById(R.id.btnOpenBarcode);
         btnOpenBarcode.setOnClickListener(new View.OnClickListener() {
@@ -40,4 +61,7 @@ public class MainActivity extends AppCompatActivity {
 
     }
 
+
 }
+
+
