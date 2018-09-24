e94cc6ec4f9dbb75c87a1fc8c476fa6645bb4495
whd0714 <33143439+whd0714@users.noreply.github.com>
2018-09-24 19:40:30 +0900

    Update RegisterActivity.java

--- app/src/main/java/com/team15x3/caucse/takecareoftherefrigerator/RegisterActivity.java
+++ app/src/main/java/com/team15x3/caucse/takecareoftherefrigerator/RegisterActivity.java
@@ -3,22 +3,46 @@ package com.team15x3.caucse.takecareoftherefrigerator;
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Build;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Patterns;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+
+import java.util.regex.Pattern;
 
 public class RegisterActivity extends AppCompatActivity {
+
+    // 비밀번호 정규식
+    private static final Pattern PASSWORD_PATTERN = Pattern.compile("^[a-zA-Z0-9!@.#$%^&*?_~]{4,16}$");
+
+    // 파이어베이스 인증 객체 생성
+    private FirebaseAuth firebaseAuth;
+    private EditText edtUserName;
+    private EditText edtPassword;
+    private EditText edtConfirmPassword;
+    private EditText edtEmail;
+    private String email = "";
+    private String password = "";
 
     Button btnRegister;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
-        if(Build.VERSION.SDK_INT >=21) {
+        firebaseAuth = FirebaseAuth.getInstance();
+        edtUserName = findViewById(R.id.edtUserName);
+        edtPassword = findViewById(R.id.edtPassword);
+        edtEmail = findViewById(R.id.edtEmail);
+      /*  if(Build.VERSION.SDK_INT >=21) {
             getWindow().setStatusBarColor(getResources().getColor(R.color.colorBackground));
         }
 
@@ -34,18 +58,18 @@ public class RegisterActivity extends AppCompatActivity {
                     finish();
                 }
             }
-        });
+        });*/
 
     }
 
-    private boolean putUserInfoToIntent(Intent intent){
+/*    private boolean putUserInfoToIntent(Intent intent){
 
         String UserName, Password, ConfirmPassword, Email;
 
-        EditText edtUserName = (EditText)findViewById(R.id.edtUserName);
-        EditText edtPassword = (EditText)findViewById(R.id.edtPassword);
-        EditText edtConfirmPassword = (EditText)findViewById(R.id.edtConfirmPassword);
-        EditText edtEmail = (EditText)findViewById(R.id.edtEmail);
+        edtUserName = (EditText)findViewById(R.id.edtUserName);
+         edtPassword = (EditText)findViewById(R.id.edtPassword);
+        edtConfirmPassword = (EditText)findViewById(R.id.edtConfirmPassword);
+        edtEmail = (EditText)findViewById(R.id.edtEmail);
 
         UserName = edtUserName.getText().toString();
         Password = edtPassword.getText().toString();
@@ -54,8 +78,67 @@ public class RegisterActivity extends AppCompatActivity {
         //check correction of information
         if(Password.equals(ConfirmPassword)){
             return true;
-        }
+    }
         return false;
 
+    }*/
+
+    public void singUp(View view) {
+        email = edtEmail.getText().toString();
+        password = edtPassword.getText().toString();
+
+        if(isValidEmail() && isValidPasswd()) {
+            createUser(email, password);
+        }
     }
+
+    public void signIn(View view) {
+        email = edtEmail.getText().toString();
+        password = edtPassword.getText().toString();
+
+
+    }
+
+    // 이메일 유효성 검사
+    private boolean isValidEmail() {
+        if (email.isEmpty()) {
+            // 이메일 공백
+            return false;
+        } else if (!Patterns.EMAIL_ADDRESS.matcher(email).matches()) {
+            // 이메일 형식 불일치
+            return false;
+        } else {
+            return true;
+        }
+    }
+
+
+    private boolean isValidPasswd() {
+        if (password.isEmpty()) {
+            // 비밀번호 공백
+            return false;
+        } else if (!PASSWORD_PATTERN.matcher(password).matches()) {
+
+            return false;
+        } else {
+            return true;
+        }
+    }
+
+    private void createUser(String email, String password) {
+        firebaseAuth.createUserWithEmailAndPassword(email, password)
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()) {
+                            // 회원가입 성공
+                            Toast.makeText(RegisterActivity.this, R.string.success_signup, Toast.LENGTH_SHORT).show();
+                        } else {
+                            // 회원가입 실패
+                            Toast.makeText(RegisterActivity.this, R.string.failed_signup, Toast.LENGTH_SHORT).show();
+                        }
+                    }
+                });
+    }
+
 }
