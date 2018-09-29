26ed1f23c70e44060e99753b6d5bfed68ac25392
rina2 <32057202+rina2@users.noreply.github.com>
2018-09-30 01:01:52 +0900

    improve splash

--- app/src/main/java/com/team15x3/caucse/takecareoftherefrigerator/LoginActivity.java
+++ app/src/main/java/com/team15x3/caucse/takecareoftherefrigerator/LoginActivity.java
@@ -4,12 +4,14 @@ package com.team15x3.caucse.takecareoftherefrigerator;
 import android.app.ProgressDialog;
 import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
 import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.text.TextUtils;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.RelativeLayout;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -32,18 +34,34 @@ public class LoginActivity extends AppCompatActivity implements View.OnClickList
     FirebaseAuth firebaseAuth;
     Button btnRegister;
 
+
+    //animation splash
+    RelativeLayout rellay1,rellay2;
+    Handler handler = new Handler();
+    Runnable runnable = new Runnable() {
+        @Override
+        public void run() {
+            rellay1.setVisibility(View.VISIBLE);
+            rellay2.setVisibility(View.VISIBLE);
+        }
+    };
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+        rellay1 =(RelativeLayout)findViewById(R.id.rellay1);
+        rellay2 =(RelativeLayout)findViewById(R.id.rellay2);
+        handler.postDelayed(runnable,2000);
 
-        //initializig firebase auth object
+          //initializig firebase auth object
         firebaseAuth = FirebaseAuth.getInstance();
 
         if(firebaseAuth.getCurrentUser() != null){
             finish();
             startActivity(new Intent(getApplicationContext(), ProfileActivity.class));
         }
+
         edtUserName = (EditText) findViewById(R.id.edtUserName);
         edtPassword = (EditText) findViewById(R.id.edtPassword);
         /*textviewSignin= (TextView) findViewById(R.id.textViewSignin);*/
